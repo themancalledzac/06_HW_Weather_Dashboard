96d79a88da29df4c058c94843de5098bd97badba
Zac Edens <themancalledzac@gmail.com>
2020-11-11 16:30:55 -0800

    started JS, fixed css so the 5 day weather now properly shows on every width.

---
+++ assets/js/js.js
@@ -0,0 +1,11 @@
+// GIVEN a weather dashboard with form inputs
+
+// create main button to save data---------------------------------------------------------------//
+var btnMain = $("#btnMain");
+btnMain.on('click', saveToStorage);
+
+// FUNCTION for saving to storage----------------------------------------------------------------//
+function saveToStorage(event) {
+    event.preventDefault();
+    let clicked = $(this).paren
+}
