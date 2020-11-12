0d77a367679eeb21754e085a1a36b392c7405a7e
Zac Edens <themancalledzac@gmail.com>
2020-11-11 17:25:38 -0800

    added github pages link to README. Also a few odd changes.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 06_HW_Weather_Dashboard
+
+https://themancalledzac.github.io/06_HW_Weather_Dashboard/
--- assets/js/js.js
+++ assets/js/js.js
@@ -7,5 +7,23 @@ btnMain.on('click', saveToStorage);
 // FUNCTION for saving to storage----------------------------------------------------------------//
 function saveToStorage(event) {
     event.preventDefault();
-    let clicked = $(this).paren
+
+    // text area data stored to a variable
+    let cityWritten = $(this).parent('div').parent('div').children('input')
+    // localStorae.setItem()
 }
+
+// Variable for button listener for all rendered cities------------------------------------------//
+var btnCities = $("")
+
+
+// FUNCTION to display cities from Saved data----------------------------------------------------//
+
+    // 5 day forecast
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+    // main forecast for city
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+    // UV index, needs Lat/Long from our main weather forcast
+// http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
